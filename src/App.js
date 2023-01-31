0eca38eed3716cf974c97c3096c2070e2bf2e8f0
Brian K Kemboi <bkemboi590@gmail.com>
2023-01-31 12:19:25 +0300

    react hooks repo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div> 
+        <h1>Hello world</h1>
+        <p>i'm from kenya</p>
+        <p>How are you</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
